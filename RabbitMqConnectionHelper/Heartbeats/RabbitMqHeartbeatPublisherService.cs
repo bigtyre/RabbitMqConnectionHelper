6714c904f1b6d45c6a6e34c5f575666a9d375297
Isaac <isaac@bigtyre.com.au>
2024-11-21 15:20:09 +1000

    Added heartbeat publisher service.

---
+++ RabbitMqConnectionHelper/Heartbeats/RabbitMqHeartbeatPublisherService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BigTyre.RabbitMq.Heartbeats
+{
+
+    public class RabbitMqHeartbeatPublisherService : BackgroundService
+    {
+        private readonly IMessagePublisher messagePublisher;
+        private readonly RabbitMqHeartbeatPublisherSettings settings;
+
+        public RabbitMqHeartbeatPublisherService(
+            IMessagePublisher messagePublisher,
+            RabbitMqHeartbeatPublisherSettings settings
+        )
+        {
+            this.messagePublisher = messagePublisher;
+            this.settings = settings;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Run(async () =>
+            {
+                var heartbeatInterval = TimeSpan.FromSeconds(settings.HeartbeatSeconds);
+
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    // logger.LogTrace("Publishing heartbeat message");
+
+                    await messagePublisher.PublishJsonMessageAsync(
+                        "heartbeats",
+                        new
+                        {
+                            settings.AppId,
+                            Time = DateTimeOffset.Now.ToString("O")
+                        }
+                    );
+
+                    await Task.Delay(heartbeatInterval, stoppingToken);
+                }
+            }, stoppingToken);
+        }
+    }
+}
---
+++ RabbitMqConnectionHelper/Heartbeats/RabbitMqHeartbeatPublisherSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace BigTyre.RabbitMq.Heartbeats
+{
+    public readonly struct RabbitMqHeartbeatPublisherSettings
+    {
+        public RabbitMqHeartbeatPublisherSettings(string appId, uint heartbeatSeconds = 2)
+        {
+            AppId = appId;
+            HeartbeatSeconds = heartbeatSeconds;
+        }
+
+        public string AppId { get; }
+        public uint HeartbeatSeconds { get; }
+    }
+}
